21be9be1114d9819fbc433d1539ce8d09cad815c
omarsakr <ifppomarsakr@gmail.com>
2024-09-14 17:45:25 +0300

    changed startdate and end date from integer to date

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,10 +1,14 @@
 import com.sun.source.tree.BreakTree;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Reservation extends BaseObject {
-    private Integer startDate;
-    private Integer endDate;
+    private Date startDate;
+    private Date endDate;
     private Double price;
     private Guest guest;
     private Room room;
@@ -27,27 +31,37 @@ public class Reservation extends BaseObject {
     public void setPrice(Double price) {
         this.price = price;
     }
-    public Integer getEndDate() {
+    public Date getEndDate() {
         return endDate;
     }
-    public void setEndDate(Integer endDate) {
+    public void setEndDate(Date endDate) {
         this.endDate = endDate;
     }
-    public Integer getStartDate() {
+    public Date getStartDate() {
         return startDate;
     }
-    public void setStartDate(Integer startDate) {
+    public void setStartDate(Date startDate) {
         this.startDate = startDate;
     }
     public void fillStartDate() {
         System.out.println("Start date");
         Scanner scanner = new Scanner(System.in);
-        startDate = scanner.nextInt();
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            startDate = dateFormat.parse(scanner.next());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
     public void fillEndDate() {
         System.out.println("End date");
         Scanner scanner = new Scanner(System.in);
-        endDate = scanner.nextInt();
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            endDate = dateFormat.parse(scanner.next());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
     public void fillPrice() {
         System.out.println("Enter price");
